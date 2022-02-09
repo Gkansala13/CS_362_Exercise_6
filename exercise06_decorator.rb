28ddf668a4d070bb582ccfe8292c3c9f1202ea54
gkansala13 <kansalag@oregonstate.edu>
2022-02-08 18:29:36 -0800

    Complete, Forgot to submit in class

--- exercise06_decorator.rb
+++ exercise06_decorator.rb
@@ -1,5 +1,5 @@
 #! /usr/bin/env ruby
-# YOUR NAME
+# GRAYSON KANSALA
 # Exercise 6: Decorator Pattern
 
 # Do not modify this class. Pretend it is from a library that is "closed."
@@ -27,19 +27,28 @@ class CarService
   end
 end
 
+class CarDecorator
+
+  def initialize(hyper_car)
+    @hyper_car = hyper_car
+  end
+
+  def go 
+    puts "awooga"
+    @hyper_car.go
+    puts "waka waka waka"
+  end
+end
+
+
 hyper_car = HyperCar.new("bzzzz")
 service = CarService.new(hyper_car)
-service.execute
+# service.execute
 
 # TODO Exercise, using the decorator pattern. Can you add new behavior without
 # changing the HyperCar class or the CarService class?
 
-
-
-
-
-
-
+second_service = CarService.new(CarDecorator.new(hyper_car))
 second_service.execute
 # This `execute` call should print:
 # > awooga!
